f247bf1a96a8040aaa20deb8fb1839bead8330ea
Ariun <thiagotatico007@gmail.com>
2022-03-29 16:37:02 -0300

    Html and css finished

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-import {
-  API_KEY, BASE_URL,
-  IMG_URL,
-  language,
-} from './api.js';
+//const axios = require('axios')
+
+import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+let random_id = getRandomInt(1, 740000)
+
+const url = `${BASE_URL}${random_id}?${API_KEY}&${language}`
+
+function getMovie() {
+  
+}
