bc538b3d519289d368b1b7d158a097dc5d564a06
Ariun <thiagotatico007@gmail.com>
2022-03-29 11:36:52 -0300

    Fisrt commit

--- api.js
+++ api.js
@@ -1,6 +1,4 @@
-// Substitua o CHAVE_DA_API com a chave gerada no site.
-
-export const API_KEY = 'api_key=CHAVE_DA_API';
-export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-export const language = 'language=pt-BR';
+export const API_KEY = 'api_key=1b8038c7c426f22348b3c4b12eb2d6d4'
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/'
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500'
+export const language = 'language=pt-BR'
