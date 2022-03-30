83b4c99ddabc5934b169230f504c81ed3894b802
Ariun <thiagotatico007@gmail.com>
2022-03-30 09:24:02 -0300

    Final changes

--- api.js
+++
@@ -1,8 +0,0 @@
-export const API_KEY = 'api_key=1b8038c7c426f22348b3c4b12eb2d6d4'
-export const BASE_URL = 'https://api.themoviedb.org/3/movie/'
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500'
-export const language = 'language=en-US'
-
-// https://api.themoviedb.org/3/movie/67667?api_key=1b8038c7c426f22348b3c4b12eb2d6d4&language=en-US
-
-// pegar  original_title, overview, poster_path 
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
+ const API_KEY = 'api_key=1b8038c7c426f22348b3c4b12eb2d6d4'
+ const BASE_URL = 'https://api.themoviedb.org/3/movie/'
+ const IMG_URL = 'https://image.tmdb.org/t/p/w500'
+ const language = 'language=en-US'
 
 function getRandomInt(min, max) {
   min = Math.ceil(min)
@@ -6,17 +9,27 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
-let random_id = getRandomInt(1, 740000)
-
-const url = `${BASE_URL}${random_id}?${API_KEY}&${language}`
-
 function getMovie() {
+  let random_id = getRandomInt(1, 700000)
+  let url = `${BASE_URL}${random_id}?${API_KEY}&${language}`
+
   axios.get(url)
     .then(response => {
-      overviews.textContent = response.data.overview
-      movie_title.textContent = response.data.original_title
-    })
-    .catch(error => console.error(error))
-}
+      let imageUrl = `${IMG_URL}${response.data.poster_path}`
 
-getMovie()
+      if(imageUrl == 'https://image.tmdb.org/t/p/w500null') {
+        movie_image.src = './assets/code.jpg'
+        movie_title.textContent = 'Ops, hoje não é dia de assistir filme. Bora codar! 🚀'
+        overviews.textContent = ''
+        return
+      }
+
+      overviews.textContent = response.data.overview
+      movie_title.textContent = response.data.original_title      
+      movie_image.src = imageUrl
+    })
+    .catch(error => {
+      console.error(error)
+      getMovie()
+    })
+}
